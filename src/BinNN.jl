bf78d3420a22860dc61e5258f079b6d276022bf2
Vaclav <vaclav.macha92@gmail.com>
2022-10-02 12:35:14 +0200

    Added binarize and binarize_stochastic

--- src/BinNN.jl
+++ src/BinNN.jl
@@ -1,3 +1,16 @@
 module BinNN
 
+using Distributions
+using ChainRulesCore
+using Flux
+
+using Broadcast: broadcasted
+using ChainRulesCore: rrule, @scalar_rule, NoTangent, project_x
+
+export binarize, binarize_stochastic
+
+const Numeric = Union{AbstractArray{<:T},T} where {T<:Number}
+
+include("binarization.jl")
+
 end # module BinNN
