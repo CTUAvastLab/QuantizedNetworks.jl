d895c4e363573b778a28ec97ba187bf0212e6420
Vaclav Macha <vaclav.macha92@gmail.com>
2022-10-11 08:45:02 +0200

    Removed all stuff related to Bop

--- src/BinNN.jl
+++ src/BinNN.jl
@@ -8,19 +8,17 @@ using NNlib
 using ChainRulesCore: rrule, @scalar_rule, NoTangent, ProjectTo
 using Flux: glorot_uniform, @functor, _create_bias
 using NNlib: hardtanh
-using Flux.Optimise: AbstractOptimiser
 
 export ClippedArray
 export value, deriv
 export AbstractEstimator, STE, PolynomialSTE, SwishSTE
 export AbstractQuantizer, Sign, Heaviside, Ternary
 export QuantDense
-export Bop
 
 include("clippedarray.jl")
 include("estimators.jl")
 include("quantizers.jl")
 include("layers/quantdense.jl")
-include("optimisers/bop.jl")
+
 
 end # module BinNN
