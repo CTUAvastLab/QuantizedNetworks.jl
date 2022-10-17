33662b83758b4586674f65571f2b8466e2c07ef3
Vaclav Macha <vaclav.macha92@gmail.com>
2022-10-17 15:35:54 +0200

    Added binarized examples

---
+++ examples/flower/train_bin.jl
@@ -0,0 +1,70 @@
+using Pkg
+Pkg.activate(@__DIR__)
+
+using Revise
+using QuantizedNetworks
+using Plots
+
+include(joinpath(@__DIR__, "utilities.jl"))
+
+# data
+dataset = "Flower"
+train, test = createloader(900, 900; batchsize = 100)
+
+# model
+input_size = size(first(train)[1], 1)
+nclasses = size(first(train)[2], 1)
+
+model = Chain(
+    Dense(input_size, 20, relu),
+    Dense(20, 20, relu),
+    Dense(20, nclasses),
+)
+
+k = 5
+weight_lims = (-1, 1)
+output_quantizer = Sign(SwishSTE())
+
+model_bin = Chain(
+    FQuantizer((input_size, k)),
+    QuantDense(k*input_size => 20, tanh; output_quantizer, weight_lims),
+    QuantDense(20 => 20, tanh; output_quantizer, weight_lims),
+    QuantDense(20 =>nclasses; output_quantizer, weight_lims),
+)
+
+# training
+epochs = 500
+
+history = train_model(model, AdaBelief(), train, test; epochs)
+history_bin = train_model(model_bin, AdaBelief(), train, test; epochs)
+
+# plots
+plt1 = plot(history.train_acc; label = "normal model", title = "Train $(dataset)")
+plot!(plt1, history_bin.train_acc; label = "binary model")
+
+plt2 = plot(history.test_acc; label = "normal model", title = "Test $(dataset)")
+plot!(plt2, history_bin.test_acc; label = "binary model")
+
+plt = plot(
+    plt1,
+    plt2;
+    layout = (2, 1),
+    size = (600, 800),
+    ylims = (0, 1),
+    xlabel = "epoch",
+    ylabel = "accuracy (%)",
+    legend = :bottomright
+)
+
+savefig(plt, "$(dataset).png")
+
+# plot
+plt_flower = plot(
+    plot_decision(model, test.data; title = "Normal model (test)"),
+    plot_decision(model_bin, test.data; title = "Binary model (test)");
+    layout = (2, 1),
+    size = (600, 800),
+    legend = false,
+)
+
+savefig(plt_flower, "$(dataset)_decision.png")
---
+++ examples/mnist/train_bin.jl
@@ -0,0 +1,57 @@
+using Pkg
+Pkg.activate(@__DIR__)
+
+using Revise
+using QuantizedNetworks
+using Plots
+
+include(joinpath(@__DIR__, "utilities.jl"))
+
+# data
+dataset = MNIST
+train, test = createloader(dataset; batchsize = 256)
+
+# model
+input_size = size(first(train)[1], 1)
+nclasses = size(first(train)[2], 1)
+
+model = Chain(
+    Dense(input_size, 32, relu),
+    Dense(32, nclasses),
+)
+
+k = 5
+weight_lims = (-1, 1)
+output_quantizer = Sign()
+
+model_bin = Chain(
+    FQuantizer((input_size, k)),
+    QuantDense(k*input_size => 32, tanh; output_quantizer, weight_lims),
+    QuantDense(32 => nclasses; output_quantizer, weight_lims),
+)
+
+# training
+epochs = 30
+
+history = train_model(model, AdaBelief(), train, test; epochs)
+history_bin = train_model(model_bin, AdaBelief(), train, test; epochs)
+
+# plots
+plt1 = plot(history.train_acc; label = "normal model", title = "Train $(dataset)")
+plot!(plt1, history_bin.train_acc; label = "binary model")
+
+plt2 = plot(history.test_acc; label = "normal model", title = "Test $(dataset)")
+plot!(plt2, history_bin.test_acc; label = "binary model")
+
+plt = plot(
+    plt1,
+    plt2;
+    layout = (2, 1),
+    size = (600, 800),
+    ylims = (0, 1),
+    xlabel = "epoch",
+    ylabel = "accuracy (%)",
+    legend = :bottomright
+)
+
+savefig(plt, "$(dataset).png")
