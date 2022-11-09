e567d26d43feb55dadd6863fa7401aee9289cd09
Vaclav <vaclav.macha92@gmail.com>
2022-11-09 16:53:46 +0100

    Added docs for quantizers and estimators

--- src/estimators.jl
+++ src/estimators.jl
@@ -1,9 +1,19 @@
+@doc raw"""
+    AbstractEstimator
+
+Estimators are used for estimation of the gradient of quantizers for whic the true gradient does not exist. Estimators are used for dispatch on backward pass, i.e. for each quantizer (for example [`Sign`](@ref)) and estimator it is necessary to define specific method for [`pullback`](@ref) function.
+"""
 abstract type AbstractEstimator end
 
+@doc raw"""
+    STE(threshold::Real = 2)
+
+For more details see [`AbstractEstimator`](@ref), [`Sign`](@ref), [`Heaviside`](@ref) or [`Ternary`](@ref).
+"""
 struct STE{T<:Real} <: AbstractEstimator
     threshold::T
 
-    function STE(threshold::T = 2) where {T<:Real}
+    function STE(threshold::T=2) where {T<:Real}
         threshold > 0 || throw(ArgumentError("`threshold` must be positive"))
         return new{T}(threshold)
     end
@@ -11,14 +21,25 @@ end
 
 Base.show(io::IO, e::STE) = print(io, "STE($(e.threshold))")
 
+@doc raw"""
+    PolynomialSTE()
+
+For more details see [`AbstractEstimator`](@ref) or [`Sign`](@ref).
+"""
 struct PolynomialSTE <: AbstractEstimator end
 
 Base.show(io::IO, ::PolynomialSTE) = print(io, "PolynomialSTE")
 
+
+@doc raw"""
+    SwishSTE(β:Real = 5)
+
+For more details see [`AbstractEstimator`](@ref) or [`Sign`](@ref).
+"""
 struct SwishSTE{T} <: AbstractEstimator
     β::T
 
-    function SwishSTE(β::T = 5) where {T<:Real}
+    function SwishSTE(β::T=5) where {T<:Real}
         β > 0 || throw(ArgumentError("`β` must be positive"))
         return new{T}(β)
     end
@@ -26,6 +47,11 @@ end
 
 Base.show(io::IO, e::SwishSTE) = print(io, "SwishSTE($(e.β))")
 
+@doc raw"""
+    StochasticSTE()
+
+For more details see [`AbstractEstimator`](@ref) or [`Sign`](@ref).
+"""
 struct StochasticSTE <: AbstractEstimator end
 
 Base.show(io::IO, ::StochasticSTE) = print(io, "StochasticSTE")
--- src/quantizers.jl
+++ src/quantizers.jl
@@ -1,4 +1,4 @@
-abstract type AbstractQuantizer{E <: AbstractEstimator} end
+abstract type AbstractQuantizer{E<:AbstractEstimator} end
 
 Base.broadcastable(q::AbstractQuantizer) = Ref(q)
 NNlib.fast_act(q::AbstractQuantizer, ::AbstractArray) = q
@@ -35,19 +35,112 @@ function ChainRulesCore.rrule(q::AbstractQuantizer, x)
     return y, quantizer_pullback
 end
 
-"""
-    Sign(estimator)
+@doc raw"""
+    Sign(estimator::AbstractEstimator = STE())
 
-deterministic binary quantizer that return -1 when the given input is less than zero and 1 otherwise.
+deterministic binary quantizer that return `-1` when the given input is less than zero or `Missing` and `1` otherwise
+```math
+sign(x) = \begin{cases}
+    -1 & x < 0 \\
+    1 & x \geq 0
+\end{cases}
+```
+The type of the inputs is preserved with exception of `Missing` input.
+
+# Estimators
+
+Estimators are used to estimate non-existing gradient of the Sign function. They are used only on backward pass.
+
+- `STE(threshold::Real = 2)`: Straight-Through Estimator approximates the sign function using the clip function
+```math
+clip(x) = \begin{cases}
+    -1 & x < \text{threshold} \\
+    1 & x > \text{threshold} \\
+    x & \text{otherwise}
+\end{cases}
+```
+with the gradient is defined as a clipped identity
+```math
+\frac{\partial clip{\partial x} = \begin{cases}
+    1 & \left|x\right| \leq \text{threshold} \\\
+    0 & \left|x\right| > \text{threshold}
+\end{cases}
+```
+- `PolynomialSTE()`: Polynomial estimater approximates the sign function using the piecewise polynomial function
+```math
+poly(x) = \begin{cases}
+    -1 & x < -1 \\
+    2x + x^2 & -1 \leq x < 0 \\
+    2x - x^2 & 0 \leq x < 1 \\
+    1 & \text{otherwise}
+\end{cases}
+```
+with the gradient is defined as
+```math
+\frac{\partial poly{\partial x} = \begin{cases}
+    2 + 2x & -1 \leq x < 0 \\
+    2 - 2x & 0 \leq x < 1 \\
+    0 & \text{otherwise}
+\end{cases}
+```
+- `SwishSTE(β=5)`: SignSwish estimator approximates the sign function using the boundes swish function
+```math
+sswish_{\beta}(x) = 2\sigma(\beta x) \left(1 + \beta x (1 - \sigma(\beta x))\right)
+```
+where $\sigma(x)$ is the sigmoid function and $\beta > 0$ controls how fast the function asymptotes to `−1` and `+1`. The gradient is defined as
+```math
+\frac{\partial sswish_{\beta}{\partial x} = \frac{\beta\left\\{2-\beta x \tanh \left(\frac{\beta x}{2}\right)\right\\}}{1+\cosh (\beta x)}
+\end{cases}
+```
 
 # References
 
 - [`Binarized Neural Networks: Training Deep Neural Networks with Weights and Activations Constrained to +1 or -1`](https://arxiv.org/abs/1602.02830)
+- [`Bi-Real Net: Enhancing the Performance of 1-bit CNNs With Improved Representational Capability and Advanced Training Algorithm`](https://arxiv.org/abs/1808.00278)
+- [`Regularized Binary Network Training`](https://arxiv.org/abs/1812.11800)
+
+# Examples
+
+```jldoctest
+julia> using QuantizedNetworks: pullback
+
+julia> x = [-2.0, -0.5, 0.0, 0.5, 1.0, missing];
+
+julia> q = Sign()
+Sign(STE(2))
+
+julia> q(x)
+6-element Vector{Float64}:
+ -1.0
+ -1.0
+  1.0
+  1.0
+  1.0
+ -1.0
+
+julia> pullback(q, x)
+6-element Vector{Float64}:
+ 1.0
+ 1.0
+ 1.0
+ 1.0
+ 1.0
+ 0.0
+
+julia> pullback(Sign(PolynomialSTE()), x)
+6-element Vector{Float64}:
+ 0.0
+ 1.0
+ 2.0
+ 1.0
+ 0.0
+ 0.0
+```
 """
 struct Sign{E<:AbstractEstimator} <: AbstractQuantizer{E}
     estimator::E
 
-    function Sign(estimator::E = STE()) where {E}
+    function Sign(estimator::E=STE()) where {E}
         return new{E}(estimator)
     end
 end
@@ -69,7 +162,7 @@ end
 
 function pullback(q::Sign{<:SwishSTE}, x::T)::T where {T<:Real}
     β = q.estimator.β
-    return (β*(2 - β*x*tanh((β*x)/2)))/(1 + cosh(β * x))
+    return (β * (2 - β * x * tanh((β * x) / 2))) / (1 + cosh(β * x))
 end
 
 function ChainRulesCore.rrule(q::Sign{<:StochasticSTE}, x)
@@ -79,15 +172,71 @@ function ChainRulesCore.rrule(q::Sign{<:StochasticSTE}, x)
     return y, Δy -> (NoTangent(), Δy)
 end
 
-"""
-    Heaviside(estimtor)
+@doc raw"""
+    Heaviside(estimator::AbstractEstimator = STE())
 
-TODO
+deterministic binary quantizer that return `0` when the given input is less than zero or `Missing` and `1` otherwise
+```math
+heaviside(x) = \begin{cases}
+    0 & x \leq 0 \\
+    1 & x > 0
+\end{cases}
+```
+The type of the inputs is preserved with exception of `Missing` input.
+
+# Estimators
+
+Estimators are used to estimate non-existing gradient of the heaviside function. They are used only on backward pass.
+
+- `STE(threshold::Real = 2)`: Straight-Through Estimator approximates the heaviside function using the clip function
+```math
+clip(x) = \begin{cases}
+    0 & x < \text{threshold} \\
+    1 & x > \text{threshold} \\
+    x & \text{otherwise}
+\end{cases}
+```
+with the gradient is defined as a clipped identity
+```math
+\frac{\partial clip{\partial x} = \begin{cases}
+    1 & \left|x\right| \leq \text{threshold} \\\
+    0 & \left|x\right| > \text{threshold}
+\end{cases}
+```
+
+# Examples
+
+```jldoctest
+julia> using QuantizedNetworks: pullback
+
+julia> x = [-2.0, -0.5, 0.0, 0.5, 1.0, missing];
+
+julia> q = Heaviside()
+Heaviside(STE(2))
+
+julia> q(x)
+6-element Vector{Float64}:
+ 0.0
+ 0.0
+ 1.0
+ 1.0
+ 1.0
+ 0.0
+
+julia> pullback(q, x)
+6-element Vector{Float64}:
+ 1.0
+ 1.0
+ 1.0
+ 1.0
+ 1.0
+ 0.0
+```
 """
 struct Heaviside{E<:AbstractEstimator} <: AbstractQuantizer{E}
     estimator::E
 
-    function Heaviside(estimator::E = STE()) where {E}
+    function Heaviside(estimator::E=STE()) where {E}
         return new{E}(estimator)
     end
 end
@@ -102,18 +251,75 @@ function pullback(q::Heaviside{<:STE}, x::T)::T where {T<:Real}
     return abs(x) <= t
 end
 
-"""
-    Ternary(Δ, estimator)
+@doc raw"""
+    Ternary(Δ::T=0.05, estimator::AbstractEstimator = STE())
 
-TODO
+deterministic ternary quantizer that return `-1` when the given input is less than `Δ`, `1` whe the input in greater than Δ, and `0` otherwise. For `Missing` input, the output is `0`.
+```math
+ternary(x) = \begin{cases}
+    -1 & x \le \Delta
+    1 & x \ge \Delta \\
+    0 & \text{otherwise}
+\end{cases}
+```
+The type of the inputs is preserved with exception of `Missing` input.
+
+# Estimators
+
+Estimators are used to estimate non-existing gradient of the ternary function. They are used only on backward pass.
+
+- `STE(threshold::Real = 2)`: Straight-Through Estimator approximates the ternary function using the clip function
+```math
+clip(x) = \begin{cases}
+    -1 & x < \text{threshold} \\
+    1 & x > \text{threshold} \\
+    x & \text{otherwise}
+\end{cases}
+```
+with the gradient is defined as a clipped identity
+```math
+\frac{\partial clip{\partial x} = \begin{cases}
+    1 & \left|x\right| \leq \text{threshold} \\\
+    -1 & \left|x\right| > \text{threshold}
+\end{cases}
+```
+
+# Examples
+
+```jldoctest
+julia> using QuantizedNetworks: pullback
+
+julia> x = [-2.0, -0.5, 0.0, 0.5, 1.0, missing];
+
+julia> q = Ternary()
+Ternary(0.05, STE(2))
+
+julia> q(x)
+6-element Vector{Float64}:
+ -1.0
+ -1.0
+  0.0
+  1.0
+  1.0
+  0.0
+
+julia> pullback(q, x)
+6-element Vector{Float64}:
+ 1.0
+ 1.0
+ 1.0
+ 1.0
+ 1.0
+ 0.0
+```
 """
-struct Ternary{E<:AbstractEstimator, T} <: AbstractQuantizer{E}
+struct Ternary{E<:AbstractEstimator,T} <: AbstractQuantizer{E}
     Δ::T
     estimator::E
 
-    function Ternary(Δ::T = 0.05, estimator::E = STE()) where {T, E}
+    function Ternary(Δ::T=0.05, estimator::E=STE()) where {T,E}
         Δ > 0 || throw(ArgumentError("`Δ` must be positive"))
-        return new{E, T}(Δ, estimator)
+        return new{E,T}(Δ, estimator)
     end
 end
 
